e59153514faa4bbfc04a31133dc09ccfcce250de
Jason Schatz <schatz@gmail.com>
2015-03-12 13:33:49 -0700

    refine the comments in kevin bacon data

--- KevinBaconData/KevinBaconData/ViewController.swift
+++ KevinBaconData/KevinBaconData/ViewController.swift
@@ -8,9 +8,23 @@
 
 import UIKit
 
+/**
+ * In this assignment you will use this view controller to 
+ * look into how JSON data can be parsed. The data in this app
+ * is in a file named "kevinBaconMoviesData". It contains the 
+ * data returned from a call to TheMovieDB.com's API, requesting
+ * the movies in which Kevin Bacon is a cast member or crew member.
+ *
+ * You can see a "pretty printed" version of this data in the file
+ * named "kevinBconMoviesPretty.txt". It might be good to briefly
+ * familiarize yourself with the data before you proceed. 
+ */
+
 class ViewController: UIViewController {
 
+    // All of the action begins in viewDidLoad. We load the data from the file, and then practice parsing it
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         var error: NSError? = nil
         
@@ -37,11 +51,13 @@ class ViewController: UIViewController {
 
             // Next, we can start making assumptions about what this JSON object contains. If you look at the 
             // file named kevinBaconMoviesPretty.txt you will see a formatted version of the JSON data. Notice
-            // that at the top level it is a dictionary with one key: "cast". That key maps to an array of 
-            // dictionary objects that represent the movies. 
+            // that at the top level it is a dictionary with two keys: "cast" and "crew". That eacj keys maps
+            // to an array of dictionary objects that represent movies.
             // 
-            // Here we carefull unwrap the result of dictionary["cast"]. If we get a value, we are off to the races
+            // Here we carefull unwrap the result of dictionary["cast"].
             if let movieArray = dictionary["cast"] as? [[String : AnyObject]] {
+                
+                // If we get a value, we are off to the races
                 processArrayOfMovieDictionaries(movieArray)
             }
         }
@@ -53,9 +69,21 @@ class ViewController: UIViewController {
      */
     
     func processArrayOfMovieDictionaries(array: [[String : AnyObject]]) {
+        question1(array)
+        question2(array)
+        // question3(array), etc...
+    }
+    
+    func question1(array: [[String : AnyObject]]) {
+        // Printing each movie dictionary in the array produces some ugly results. We can do better.
         
-        // Printing the entire array produces some ugly results in the console window. We can do better.
-        println(array)
+        for m in array {
+            println(m)
+        }
+    }
+    
+    func question2(array: [[String : AnyObject]]) {
+        // your code here...
     }
 }
 
