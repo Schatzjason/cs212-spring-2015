01f2d3e3532a7dd32a083fc148fe333d7774765f
schatzjason <jason.schatz@udacity.com>
2015-04-07 16:02:07 -0700

    added Persistence

---
+++ Persistence/Persistence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Persistence
+//
+//  Created by Jason on 4/7/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+}
+
