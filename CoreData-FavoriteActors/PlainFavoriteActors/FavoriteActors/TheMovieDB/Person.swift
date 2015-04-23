4d22e7c85eedbd972835cfa6813da01c04b986cd
Jason Schatz <schatz@gmail.com>
2015-04-23 10:20:55 -0700

    add comments to Person.swift

--- CoreData-FavoriteActors/PlainFavoriteActors/FavoriteActors/TheMovieDB/Person.swift
+++ CoreData-FavoriteActors/PlainFavoriteActors/FavoriteActors/TheMovieDB/Person.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+
+// Step 1: Add import for core data
 import CoreData
 
+// Step 2: add this strange @objc(Person) line. An annotation that allows Objective-C code
+// in core data to "see" this class.
 @objc(Person)
 
+// Step 3: Make Person a subclass of NSManagedObject
 class Person: NSManagedObject {
     
     struct Keys {
@@ -19,16 +24,19 @@ class Person: NSManagedObject {
         static let ID = "id"
     }
     
+    // Step 4: Put the NSManaged annotation in front of each property
     @NSManaged var name: String
     @NSManaged var id: Int
     @NSManaged var imagePath: String?
     @NSManaged var movies: [Movie]
     
+    // Step 6: Override  the two argument init method
     override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
         
         super.init(entity: entity, insertIntoManagedObjectContext: context)
     }
     
+    // Step 7: Create the convenience init that accepts a context and a dictionary
     convenience init(context: NSManagedObjectContext, dictionary: [String : AnyObject]) {
     
         let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: context)!
